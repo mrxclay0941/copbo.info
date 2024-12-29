0d6a928c8a10cdc0b41e4e5d54142af4780b2698
Vi Van Diem <92796402+mrxclay0941@users.noreply.github.com>
2024-12-29 23:46:46 +0700

    Update animesion.js

--- animesion.js
+++ animesion.js
@@ -1,12 +1,12 @@
-// click để phát nhạc 
 window.addEventListener('click',() => {
   document.getElementById("song").play();
 })
+
 // Lấy lớp phủ
 const overlay = document.getElementById('overlay');
 
 // Lắng nghe sự kiện click vào lớp phủ
 overlay.addEventListener('click', function() {
   // Ẩn lớp phủ khi click vào
-  overlay.style.display = 'none';
+overlay.style.display = 'none';
 });
